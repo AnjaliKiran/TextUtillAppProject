d651c855eb7d28d2a0b2e5487e98a06c86405839
AnjaliKiran <55487588+AnjaliKiran@users.noreply.github.com>
2024-07-28 11:09:36 +0530

    Add files via upload

---
+++ TextUtill-React-App/src/App.js
@@ -0,0 +1,80 @@
+// import logo from './logo.svg';
+import { useState } from "react";
+import "./App.css";
+import Alert from "./components/Alert";
+// import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "dark") {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode enabled", "success");
+      // document.title = "TextUtill - Light Mode";
+    } else {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode enabled", "success");
+      // document.title = "TextUtill - Dark Mode";
+    }
+  };
+  return (
+    <>
+      {/* <div className="container my-3"> */}
+      {/* <TextForm
+          showAlert={showAlert}
+          heading="Enter the text to analayze"
+          mode={mode}
+        />
+        <About /> */}
+      {/* <BrowserRouter> */}
+      <Navbar
+        title="TextUtils"
+        // about="About TextUtils"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        {/* <Routes> */}
+        {/* <Route exact */}
+        {/* path="/" */}
+        {/* element={ */}
+        <TextForm
+          showAlert={showAlert}
+          toggleMode={toggleMode}
+          heading="Try Textutil - Word Counter, Character counter, Case convertor"
+          mode={mode}
+        />
+        {/* } */}
+        {/* /> */}
+        {/* <Route exact path="/about" element={<About />} /> */}
+
+        {/* </Routes> */}
+      </div>
+      {/* </BrowserRouter> */}
+
+      {/* </div> */}
+    </>
+  );
+}
+
+export default App;
